8ee4b18c43a57293bcdf40ad299c2708c93043e9
andreafgomezs <andreafgomez@unicauca.edu.co>
2026-06-04 10:28:24 -0500

    Delete availability-service/src/main directory

--- availability-service/src/main/java/com/groupsoft/piedrazul/availability/application/dto/DoctorRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.groupsoft.piedrazul.availability.application.dto;
-
-import lombok.Data;
-
-@Data
-public class DoctorRequestDTO {
-    private String fullName;
-    private String specialty;
-}
--- availability-service/src/main/java/com/groupsoft/piedrazul/availability/domain/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groupsoft.piedrazul.availability.domain.repository;
-
-import com.groupsoft.piedrazul.availability.domain.model.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
