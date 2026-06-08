3be43243db3cb7a252c1924e741779dce241d84d
sarahquintero <161500631+sarahquintero@users.noreply.github.com>
2026-06-07 21:06:20 -0500

    Enhance Gender enum with descriptive comments
    Added comments to clarify the purpose of the Gender enum values.

--- user-service/src/main/java/com/groupsoft/piedrazul/user/domain/model/Gender.java
+++ user-service/src/main/java/com/groupsoft/piedrazul/user/domain/model/Gender.java
@@ -1,7 +1,12 @@
 package com.groupsoft.piedrazul.user.domain.model;
+// Paquete del dominio User, contiene modelos y enums relacionados con usuarios
 
+/**
+ * Enum que representa el género de un usuario.
+ * Se utiliza en la entidad User y en los DTOs para definir el género.
+ */
 public enum Gender {
-    HOMBRE,
-    MUJER,
-    OTRO
+    HOMBRE, // Género masculino
+    MUJER,  // Género femenino
+    OTRO    // Otro género o no especificado
 }
